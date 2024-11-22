49ea56c8fc4285e2f0a857ef571a31f57d506e04
RenatoPereirals <renatopreirals@gmail.com>
2024-11-21 22:54:08 -0300

    ci: ci testing

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StockSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Stock Simulator!");
+        }
+    }
+}
