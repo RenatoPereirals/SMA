73c174a712421b9d41f8dab4f72a8e96c91b1008
RenatoPereirals <renatopreirals@gmail.com>
2024-11-23 22:33:46 -0300

    feat(helpers): add DateHelper for parsing dates with multiple formats
    - Implemented `DateHelper` class with a `ParseDate` method to handle date parsing.
    - Supports multiple date formats including "dd/MM/yyyy", "MM/dd/yyyy", "d/M/yyyy", and "yyyy-MM-dd".
    - Includes error handling for null or empty date strings and invalid formats.
    - Uses `pt-BR` culture for proper date formatting and parsing.

---
+++ src/StockSimulator.API/Helpers/DateHelpers.cs
@@ -0,0 +1,39 @@
+using System.Globalization;
+
+namespace StockSimulator.API.Helpers
+{
+    public class DateHelper
+    {
+        private readonly CultureInfo _culture;
+
+        public DateHelper()
+        {
+            _culture = new CultureInfo("pt-BR");
+        }
+
+        public DateTime ParseDate(string date)
+        {
+            if (string.IsNullOrEmpty(date))
+            {
+                Console.WriteLine("The date field is required.");
+                throw new ArgumentNullException(nameof(date), "The date field is required.");
+            }
+
+            try
+            {
+                string dateDecoded = System.Net.WebUtility.UrlDecode(date);
+                string[] formats = { "dd/MM/yyyy", "MM/dd/yyyy", "d/M/yyyy", "yyyy-MM-dd" };
+
+                if (DateTime.TryParseExact(dateDecoded, formats, _culture, DateTimeStyles.None, out DateTime parsedDate))
+                    return parsedDate;
+                else
+                    throw new FormatException("Invalid date format");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to parse date: {ex.Message}");
+                throw new FormatException("Failed to parse date");
+            }
+        }
+    }
+}
