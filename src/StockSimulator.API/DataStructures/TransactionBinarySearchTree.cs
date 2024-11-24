2efeb018a2d4e207d5abeeb7d7c5c1381598a719
RenatoPereirals <renatopreirals@gmail.com>
2024-11-23 22:28:49 -0300

    feat(api): add TransactionBinarySearchTree for transaction storage and retrieval
    - Implemented `TransactionBinarySearchTree` class to organize transactions using a binary search tree structure.
    - Added `Insert` method for adding transactions, ordered by their timestamp.
    - Introduced `Search` method to retrieve transactions by timestamp efficiently.
    - Included a nested `Node<T>` class to represent tree nodes, supporting generic data storage.
    - Ensures null safety with `ArgumentNullException.ThrowIfNull` in the search logic.

---
+++ src/StockSimulator.API/DataStructures/TransactionBinarySearchTree.cs
@@ -0,0 +1,56 @@
+
+
+using StockSimulator.API.API.Entities;
+
+namespace StockSimulator.API.DataStructures;
+
+public class TransactionBinarySearchTree
+{
+    private Node<Transaction>? root;
+
+    // Insert a transaction in the tree
+    public void Insert(Transaction transaction)
+    {
+        root = InsertRec(root, transaction);
+    }
+
+    private static Node<Transaction> InsertRec(Node<Transaction>? root, Transaction transaction)
+    {
+        if (root == null)
+        {
+            root = new Node<Transaction>(transaction);
+            return root;
+        }
+
+        if (transaction.Timestamp < root.Data.Timestamp)
+            root.Left = InsertRec(root.Left, transaction);
+        else
+            root.Right = InsertRec(root.Right, transaction);
+
+        return root;
+    }
+
+    // Search a transaction by timestamp
+    public Transaction Search(DateTime timestamp)
+    {
+        return SearchRec(root, timestamp);
+    }
+
+    private static Transaction SearchRec(Node<Transaction>? root, DateTime timestamp)
+    {
+        ArgumentNullException.ThrowIfNull(root);
+
+        if (root.Data.Timestamp == timestamp)
+            return root.Data;
+
+        return timestamp < root.Data.Timestamp ?
+            SearchRec(root.Left, timestamp) : SearchRec(root.Right, timestamp);
+    }
+}
+
+public class Node<T>(T data)
+{
+    public T Data { get; set; } = data;
+public Node<T>? Left { get; set; } = null;
+public Node<T>? Right { get; set; } = null;
+}
