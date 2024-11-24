834ef6d585a8daecfcc023b72ee3c205e9668dad
RenatoPereirals <renatopreirals@gmail.com>
2024-11-23 22:29:13 -0300

    feat(api): add TransactionLinkedList for sequential transaction management
    - Implemented `TransactionLinkedList` class to manage transactions using a linked list.
    - Added `AddTransaction` method to append transactions to the end of the list.
    - Utilizes `LinkedList<Transaction>` for efficient sequential data storage.

---
+++ src/StockSimulator.API/DataStructures/TransactionLinkedList.cs
@@ -0,0 +1,16 @@
+
+using StockSimulator.API.API.Entities;
+
+namespace StockSimulator.API.DataStructures
+{
+    public class TransactionLinkedList
+    {
+        private readonly LinkedList<Transaction> _transactions = new();
+
+        // Adding a transaction to the linked list
+        public void AddTransaction(Transaction transaction)
+        {
+            _transactions.AddLast(transaction);
+        }
+    }
+}
