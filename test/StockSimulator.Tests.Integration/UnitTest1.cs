86380f89e59b39773fbf6dc952be214cc5f29661
RenatoPereirals <renatopreirals@gmail.com>
2024-11-23 23:05:21 -0300

    ci: fix CI pipeline by ensuring proper paths for project and test execution

---
+++ test/StockSimulator.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace StockSimulator.Tests.Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var expected = 1;
+
+        // Act
+        var actual = 1;
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ test/StockSimulator.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace StockSimulator.Tests.Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var expected = 1;
+
+        // Act
+        var actual = 1;
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
