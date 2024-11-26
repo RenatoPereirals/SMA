c381bde7e88061a106a9d01b9b0f8dd1a9d56331
RenatoPereirals <renatopreirals@gmail.com>
2024-11-25 22:39:31 -0300

    feat(api): add MarketService for stock price updates and buying/selling functionality - Created `MarketService` class for managing stock prices and transactions. - Implemented `UpdateAllStockPrices` to simulate price fluctuations within a range of -5% to +5% for all stocks. - Added `CalculateProfitOrLoss` method to calculate the profit or loss for a user based on their portfolio and stock market prices. - Implemented `BuyStockFromMarket` and `SellStockToMarket` methods for handling stock purchases and sales from the market, including stock quantity validation. - Introduced `StockPurchaseResult` class for handling purchase/sale outcomes with status and remaining stock quantity.

--- src/StockSimulator.API/Services/MarketService.cs
+++ src/StockSimulator.API/Services/MarketService.cs
@@ -1,21 +1,15 @@
 
+using StockSimulator.API.Data;
 using StockSimulator.API.Entities;
+using StockSimulator.API.Services.Results;
 
 namespace StockSimulator.API.Services;
 
-public class MarketService(ICollection<Stock> listStocks, UserServices userServices)
+public class MarketService(ICollection<Stock> listStocks)
 {
     private readonly ICollection<Stock> _listStocks = listStocks ?? throw new ArgumentNullException(nameof(listStocks));
-    private readonly UserServices _userServices = userServices ?? throw new ArgumentNullException(nameof(userServices));
 
-    // Method to add a stock to the market
-    public void AddStock(Stock stock)
-    {
-        _listStocks.Add(stock);
-    }
-
-    // Method to update the prices of all stocks
-    public void UpdatePrices()
+    public void UpdateAllStockPrices()
     {
         foreach (var stock in _listStocks)
         {
@@ -24,28 +18,59 @@ public class MarketService(ICollection<Stock> listStocks, UserServices userServi
             decimal fluctuation = (decimal)(rand.NextDouble() * 0.1 - 0.05);
             stock.Price += stock.Price * fluctuation;
 
-            // Ensure the price is not negative
-            if (stock.Price < 0)
-                stock.Price = 0;
+            stock.Price = stock.Price < 0 ? 0 : stock.Price; // Ensure price does not go negative
 
-            stock.Timestamp = DateTime.Now;  // Update the fluctuation date
+            stock.Timestamp = DateTime.Now;
         }
     }
 
-// Method to calculate profit or loss of a user    
-    public decimal CalculateProfitOrLoss(User user)
+    public StockPurchaseResult BuyStockFromMarket(Stock stock, int quantity)
     {
-        decimal totalProfitOrLoss = 0;
+        if (stock == null)
+            throw new ArgumentNullException(nameof(stock), "The stock cannot be null.");
 
-        foreach (var stock in user.GetPortfolio(user))
+
+        if (quantity <= 0)
+            throw new ArgumentException("The quantity of stocks must be greater than zero.");
+
+        var existingStock = StockList.Stocks.FirstOrDefault(s => s == stock);
+
+        if (existingStock == null)
+            return new StockPurchaseResult(false, "Stock not found in the market.", 0);
+
+        bool sufficientStocks = existingStock.Quantity >= quantity;
+
+        if (!sufficientStocks)
+            return new StockPurchaseResult(false, "Insufficient stock quantity in the market.", existingStock.Quantity);
+
+        existingStock.Quantity -= quantity;
+        return new StockPurchaseResult(true, "Stock successfully purchased.", existingStock.Quantity);
+    }
+
+    public StockPurchaseResult SellStockToMarket(Stock stock, int quantity)
+    {
+        if (stock == null)
+            throw new ArgumentNullException(nameof(stock), "The stock cannot be null.");
+
+
+        if (quantity <= 0)
+            throw new ArgumentException("The quantity of stocks must be greater than zero.");
+
+        var existingStock = StockList.Stocks.FirstOrDefault(s => s == stock);
+
+        if (existingStock == null)
         {
-            decimal currentMarketValue = stock.Price * stock.Quantity;
-
-            decimal totalInvestment = _userServices.GetTotalInvestedInStock(user, stock.Symbol);
-
-            totalProfitOrLoss += currentMarketValue - totalInvestment;
+            AddStockToMarket(stock);
+            existingStock = stock;
         }
 
-        return totalProfitOrLoss;
+        existingStock.Quantity += quantity;
+
+        return new StockPurchaseResult(true, "Stock successfully sold.", existingStock.Quantity);
+    }
+
+    private static void AddStockToMarket(Stock stock)
+    {
+        StockList.Stocks.Add(stock);
     }
 }
