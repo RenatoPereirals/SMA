c86f9887b4554f82284271774a45689dcf1612a6
RenatoPereirals <renatopreirals@gmail.com>
2024-11-25 22:35:01 -0300

    refactor(api): refactor Transaction class for better consistency and validation - Updated `Transaction` class with nullable `Stock` property. - Introduced additional constructor to handle transaction types with `Amount` instead of `Stock` and `Quantity`. - Enhanced validation for `Amount` to ensure it's greater than zero. - Fixed `TotalValue` to `Amount` and adjusted method names accordingly for clarity. - Refined deposit and withdrawal methods for improved logic and validation.

--- src/StockSimulator.API/Entities/Transaction.cs
+++ src/StockSimulator.API/Entities/Transaction.cs
@@ -1,16 +1,15 @@
 
-using StockSimulator.API.Entities;
 using StockSimulator.API.Enums;
 
-namespace StockSimulator.API.API.Entities;
+namespace StockSimulator.API.Entities;
 
 public class Transaction
 {
     public Guid TransactionId { get; set; }
     public TransactionType Type { get; }  // None, Buy or Sell
-    public Stock Stock { get; }
+    public Stock? Stock { get; }
     public int Quantity { get; }
-    public decimal TotalValue { get; }
+    public decimal Amount { get; }
     public User User { get; }
     public DateTime Timestamp { get; }
 
@@ -20,44 +19,23 @@ public class Transaction
 
         if (quantity <= 0)
             throw new ArgumentException("The quantity of stocks must be greater than zero.");
-            
+
         User = user ?? throw new ArgumentNullException(nameof(user), "The user cannot be null.");
         Stock = stock ?? throw new ArgumentNullException(nameof(stock), "The stock cannot be null.");
 
         Quantity = quantity;
-        TotalValue = stock.Price * quantity;
+        Amount = stock.Price * quantity;
         Timestamp = DateTime.Now;
     }
 
-    public static void GetTransactionSummary(List<Transaction> transactions)
+    public Transaction(TransactionType type, User user, decimal amount)
     {
-        var total = transactions.Sum(t => t.TotalValue);
+        if (Amount <= 0)
+            throw new ArgumentException("The Amount must be greater than zero.");
 
-        foreach (var transaction in transactions)
-        {
-            var stock = transaction.Stock;
-
-            Console.WriteLine($"{transaction.Timestamp} - {transaction.Type} {transaction.Quantity}" +
-            $" {stock.Symbol} at {stock.Price:C}");
-        }
-    }
-
-    // Method to deposit an amount into the account
-    public void Deposit(User user, decimal amount)
-    {
-        if (amount <= 0) throw new ArgumentException("Deposit amount must be positive.");
-        user.SetBalance(amount);
-    }
-
-    // Method to withdraw an amount from the account
-    public bool Withdraw(User user, decimal amount)
-    {
-        var balance = user.GetBalance(user);
-
-        if (amount <= 0) throw new ArgumentException("Withdraw amount must be positive.");
-
-        if (amount > balance) return false; // Insufficient balance
-        balance -= amount;
-        return true;
-    }
+        Type = type;
+        User = user;
+        Amount = amount;
+    }   
 }
+
