53e4ec31e560b52048a557c472958b21c8437208
RenatoPereirals <renatopreirals@gmail.com>
2024-11-25 22:36:27 -0300

    refactor(api): update User class with financial account and transaction management - Introduced `FinancialAccount` to manage user's financial balance separately from the main `Balance` property. - Added `Portfolio` property to track user's stock portfolio. - Refined user-related methods to improve validation and consistency, including balance checks and portfolio management. - Added `AddTransaction` method to record transactions and store them in the `_transactions` list. - Updated constructor to initialize `FinancialAccount` and `Portfolio` for each user. - Enhanced methods to retrieve user account and portfolio details.

--- src/StockSimulator.API/Entities/User .cs
+++ src/StockSimulator.API/Entities/User .cs
@@ -1,56 +1,36 @@
+
+using StockSimulator.API.Entities;
+using StockSimulator.API.Enums;
+
 namespace StockSimulator.API.Entities;
 
-public class User(string firstName, string lastName, Account account, decimal initialBalance)
+public class User
 {
+    private readonly List<Transaction> _transactions = new List<Transaction>() ?? throw new ArgumentNullException(nameof(_transactions));
+
     public Guid UserId { get; private set; } = Guid.NewGuid();
-    public string FirstName { get; private set; } = firstName;
-    public string LastName { get; private set; } = lastName;
-    public string FullName { get; private set; } = $"{firstName} {lastName}";
-    public decimal Balance { get; private set; } = initialBalance >= 0 ? initialBalance : throw new ArgumentException("Initial balance cannot be negative.");
-    public Account Account { get; private set; } = account;
-    private List<Stock> Portfolio { get; set; } = [];
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string FullName { get; private set; }
+    public decimal Balance { get; private set; }
+    public IReadOnlyList<Transaction> Transactions => _transactions.AsReadOnly();
+    public FinancialAccount FinancialAccount { get; set; }
+    public Account Account { get; private set; }
+    public Portfolio Portfolio { get; set; }
 
-    // Method to get user by Id
-    public Guid GetUserById(Guid userId)
+    public User(string firstName, string lastName, Account account, decimal initialBalance)
     {
-        ArgumentNullException.ThrowIfNull(userId, nameof(userId));
-
-        if (userId == UserId)
-        {
-            return userId;
-        }
-        else
-        {
-            throw new Exception("User not found.");
-        }
-    }
-
-    // Method to add a stock to the user's portfolio
-    public void AddStockToPortfolio(Stock stock)
-    {
-        Portfolio.Add(stock);
-    }
-
-    // Method to remove a stock from the user's portfolio
-    public void RemoveStockFromPortfolio(Stock stock)
-    {
-        Portfolio.Remove(stock);
-    }
-
-    // Method to get the user's full name
-    public string GetFullName()
-    {
-        return FullName;
-    }
-
-    // Method to get the user's portfolio
-    public ICollection<Stock> GetPortfolio(User user)
-    {
-        return user.Portfolio;
+        FirstName = firstName;
+        LastName = lastName;
+        FullName = $"{firstName} {lastName}";
+        Balance = initialBalance >= 0 ? initialBalance : throw new ArgumentException("Initial balance cannot be negative.");
+        FinancialAccount = new FinancialAccount(initialBalance);
+        Account = account;
+        Portfolio = new Portfolio(UserId);
     }
 
     //Method to get the user's account
-    public Account GetUserAccount(User user)
+    public Account GetUserFinancialAccount(User user)
     {
         return user.Account;
     }
@@ -67,16 +47,10 @@ public class User(string firstName, string lastName, Account account, decimal in
         return user.Balance;
     }
 
-    // Method to withdraw money from the account
-    public decimal GetPortfolioValue(User user)
+    public void AddTransaction(Transaction transaction)
     {
-        decimal totalValue = 0;
-        var portfolio = GetPortfolio(user);
-
-        foreach (var stock in portfolio)
-            totalValue += stock.Price * stock.Quantity;
-
-        return totalValue;
+        ArgumentNullException.ThrowIfNull(transaction);
+        _transactions.Add(transaction);
     }
 }
 
